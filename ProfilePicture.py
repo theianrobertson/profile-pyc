266704fedddf15e83ed701055ba53760a32b81b3
Ian Robertson <ian.robertson@capitalone.com>
2016-11-15 21:34:07 -0500

    quick fix of a few things

--- ProfilePicture.py
+++ ProfilePicture.py
@@ -1,99 +1,106 @@
-#Imports in some packages.
-import mechanize
-import cookielib
+"""
+Script to download a newspaper image and paste it into a profile picture.
+"""
+
+import base64
 import datetime
+import json
+import os
+import random
+import shutil
+
+import requests
 from PIL import Image, ImageFilter
 from twython import Twython
-import base64
-import os
 
-def newspaper_file(filename):
-    '''
-    Reaches out to a newspaper and grabs a picture.
-    '''
 
-    # Opens up a browser instance in mechanize 
-    br = mechanize.Browser()
-    cj = cookielib.LWPCookieJar()
-    br.set_cookiejar(cj)
-    
-    # Set how the mechanize browser handles some options
-    br.set_handle_equiv(True)
-    br.set_handle_gzip(True)
-    br.set_handle_redirect(True)
-    br.set_handle_referer(True)
-    br.set_handle_robots(False)
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        
+def get_newspaper_file(filename, slug='CAN_TGAM'):
+    """
+    Reach out to a newspaper, grab a picture and save it.
+    """
+
     #This is hard-wired to grab the Globe and Mail.
-    dater = int(datetime.date.today().strftime("%d"))
-    img_url = 'http://webmedia.newseum.org/newseum-multimedia/dfp/jpg{0}/lg/CAN_TGAM.jpg'.format(dater)
-    
-    print 'Getting url: {0}'.format(img_url)
-    data = br.open(img_url).read()
-    save = open(filename, 'wb')
-    save.write(data)
-    save.close()
+    date_str = int(datetime.date.today().strftime("%d"))
+    img_url = (
+        'http://webmedia.newseum.org/newseum-multimedia/dfp/jpg{0}'
+        '/lg/{1}.jpg').format(date_str, slug)
+
+    print 'Getting url: ' + img_url
+    resp = requests.get(img_url, stream=True)
+    if resp.status_code == 200:
+        with open(filename, 'wb') as out_file:
+            resp.raw.decode_content = True
+            shutil.copyfileobj(resp.raw, out_file)
     print 'Got it!'
 
 
 def splice_image(newspaper_file, base_file, new_file):
-    '''
-    Splices a new newspaper cover into the base picture
-    '''
-    
+    """
+    Splice a new newspaper cover into the base picture.  Full of magic numbers.
+    """
+
     print 'Splicing...'
     #Opens the two images:
-    im = Image.open(base_file)
-    impage = Image.open(newspaper_file)
+    base_image = Image.open(base_file)
+    page_image = Image.open(newspaper_file)
 
     #Blurs
-    imblur = impage.filter(ImageFilter.GaussianBlur(radius=1))
+    page_blurred = page_image.filter(ImageFilter.GaussianBlur(radius=1))
+    page_blurred = page_blurred.resize(700, 1409)
 
-    #Now splice together and save
-    mask = impage.convert('L')
+    #Create a mask (invisible if >= 240 in greyscale)
+    mask = page_image.convert('L')
     mask = Image.eval(mask, lambda x: 0 if x >= 240 else 255)
-    im.paste(imblur, (950, 110), mask=mask)
-    imResize = im.resize((597,600), Image.ANTIALIAS)
-    imResize.save(new_file, 'JPEG', quality=90)
+
+    #Paste into the base image and save.
+    base_image.paste(page_blurred, (950, 110), mask=mask)
+    to_save = base_image.resize((597, 600), Image.ANTIALIAS)
+    to_save.save(new_file, 'JPEG', quality=90)
     print 'Spliced!'
 
 
 def post_to_twitter(filename):
-    '''
+    """
     Posts the actual image to the twitters.
-    Requires a keys.json file, with app_key
-    '''
+    Requires a keys.json file saved in the same directory, which looks like:
+    {
+        "app_key": "my_app_key",
+        "app_secret": "my_app_secret",
+        "oauth_token": "my_oauth_token",
+        "oauth_token_secret": "my_oauth_token_secret"
+    }
+    """
 
     print 'Posting to twitter'
-    with open('keys.json') as f:
-        keys = eval(f.read())
-    
+    with open('keys.json') as key_file:
+        keys = json.load(key_file)
+
     #Grabs the connection to twitter
-    twitter = Twython(keys['app_key'],
-                      keys['app_secret'],
-                      keys['oauth_token'],
-                      keys['oauth_token_secret'])
-    
+    twitter = Twython(
+        keys['app_key'], keys['app_secret'], keys['oauth_token'],
+        keys['oauth_token_secret'])
+
     with open(filename, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-    
-    i = twitter.update_profile_image(image=encoded_string)
+
+    _ = twitter.update_profile_image(image=encoded_string)
     print 'Posted!'
 
 
 if __name__ == "__main__":
 
-    news_file = 'img/' + datetime.date.today().strftime("%Y%m%d") + '.jpg'
-    new_file = 'newimage.jpg'
-    base_file = 'baseimage.jpg'
-    
-    #Using this as a scheduled task, when the computer is on, then just run it a bunch of times every day I guess.
-    if os.path.exists(news_file):
+    NEWS_FILE = 'img/' + datetime.date.today().strftime("%Y%m%d") + '.jpg'
+    NEW_FILE = 'newimage.jpg'
+    BASE_FILE = 'baseimage.jpg'
+    SLUG_CHOOSER = ('AZ_AR', 'CAN_TGAM')
+    RANDOM_SLUG = random.choice(SLUG_CHOOSER)
+
+    #Using this as a scheduled task, when the computer is on, then just
+    #run it a bunch of times every day.
+    if os.path.exists(NEWS_FILE):
         print 'Already done!!!'
-        pass
     else:
         #Grabs the stuff
-        newspaper_file(news_file)
-        splice_image(news_file, base_file, new_file)
-        post_to_twitter(new_file)
+        get_newspaper_file(NEWS_FILE, slug=RANDOM_SLUG)
+        splice_image(NEWS_FILE, BASE_FILE, NEW_FILE)
+        post_to_twitter(NEW_FILE)
